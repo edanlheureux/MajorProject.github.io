31dc7be4431470b84b0f00d3fe25a5514dae41aa
edanlheureux <goldenblood665@gmail.com>
2018-12-13 14:10:37 -0600

    Update-1-Music/Images

---
+++ p5/OregonTrail.js
@@ -0,0 +1,72 @@
+// Oregon Trail
+// Edan L'Heureux
+// --/--/2019
+
+let food;
+let clothes;
+
+let music,music1,music2,music3;
+let music4,music5,music6,music7;
+let music8,music9,music10,music11;
+let music12,music13,music14,music15;
+let music16,music17,music18;
+
+let img,img1,img2,img3;
+let img4,img5,img6,img7;
+let img8,img9,img10,img11;
+let img12,img13,img14,img15;
+let img16,img17,img18;
+
+
+
+function preload(){
+  music = loadSound("assets/Oregon-Trail-Title-Screen1.mp3");
+  music1 = loadSound("assets/Oregon-Trail-independence-MO2.mp3");
+  music2 = loadSound("assets/Oregon-Trail-Kansas-River Crossing3.mp3");
+  music3 = loadSound("assets/Oregon-Trail-Big-Blue River Crossing4.mp3");
+  music4 = loadSound("assets/Oregon-Trail-Fort-Kearney5.mp3");
+  music5 = loadSound("assets/Oregon-Trail-Chimney-Rock6.mp3");
+  music6 = loadSound("assets/Oregon-Trail-Fort-Laramie7.mp3");
+  music7 = loadSound("assets/Oregon-Trail-independence-Rock8.mp3");
+  music8 = loadSound("assets/Oregon-Trail-South-Pass9.mp3");
+  music9 = loadSound("assets/Oregon-Trail-");
+  music10 = loadSound("assets/Oregon-Trail-Green-River-Crossing11.mp3");
+  music11 = loadSound("assets/Oregon-Trail-Soda-Springs12.mp3");
+  music12 = loadSound("assets/Oregon-Trail-");
+  music13 = loadSound("assets/Oregon-Trail-");
+  music14 = loadSound("assets/Oregon-Trail-");
+  music15 = loadSound("assets/Oregon-Trail-");
+  music16 = loadSound("assets/Oregon-Trail-");
+  music17 = loadSound("assets/Oregon-Trail-");
+  music18 = loadSound("assets/Oregon-Trail-");
+  ///////////////////////////////////////////////////////////////////////////
+  img = loadImage("assets/Menu.png");
+  img1 = loadImage("assets/independence.PNG");
+  img2 = loadImage("assets/Kansas-River.PNG");
+  img3 = loadImage("assets/Big-Blue-River.PNG");
+  img4 = loadImage("assets/Fort-Kearney.PNG");
+  img5 = loadImage("assets/Chimney-Rock.PNG");
+  img6 = loadImage("assets/Fort-laramie.PNG");
+  img7 = loadImage("assets/independance.PNG");
+  img8 = loadImage("assets/South-Pass.PNG");
+  img9 = loadImage("assets/Green-River-crossing.PNG");
+  img10 = loadImage("assets/Soda-Springs.PNG");
+  img11 = loadImage("assets/");
+  img12 = loadImage("assets/");
+  img13 = loadImage("assets/");
+  img14 = loadImage("assets/");
+  img15 = loadImage("assets/");
+  img16 = loadImage("assets/");
+  img17 = loadImage("assets/");
+  img18 = loadImage("assets/");
+}
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
